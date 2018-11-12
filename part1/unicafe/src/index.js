b504db06b6c852e3a899e14a9e3eef5682f72fe1
jkaari96 <jaakkoakaariainen@gmail.com>
2018-11-12 12:12:33 +0200

    Exercise 1.10 finished

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -20,142 +20,93 @@ const Statistic = (props) => {
 }
 
 const Statistics = (props) => {
-  if (props.tila[0].value === 0 && props.tila[1].value === 0 &&
-      props.tila[2].value === 0){
+  if (props.tila.hyva === 0 && props.tila.neutraali === 0 &&
+      props.tila.huono === 0){
         return (
           <div>
-            <Otsikko name="statistiikka"/>
             <p>ei yhtään palautetta annettu</p>
           </div>
         )
   } else {
     return (
       <div>
-        <Otsikko name="statistiikka"/>
         <ul>
-          <Statistic name={props.tila[0].nimi}
-            value={props.tila[0].value} />
-          <Statistic name={props.tila[1].nimi}
-            value={props.tila[1].value} />
-          <Statistic name={props.tila[2].nimi}
-            value={props.tila[2].value} />
-          <Statistic name={props.tila[3].nimi}
-            value={props.tila[3].value.toFixed(1)} />
-          <Statistic name={props.tila[4].nimi}
-            value={props.tila[4].value.toFixed(1) + "%"} />
+          <Statistic name="hyvä"
+            value={props.tila.hyva} />
+          <Statistic name="neutraali"
+            value={props.tila.neutraali} />
+          <Statistic name="huono"
+            value={props.tila.huono} />
+          <Statistic name="keskiarvo"
+            value={props.funktiot.laskeKeskiarvo().toFixed(1)} />
+          <Statistic name="positiivisia"
+            value={props.funktiot.laskePositiivistenOsuus().toFixed(1) + "%"} />
         </ul>
       </div>
     )
   }
 }
 
-const Palaute = (props) => {
-  return (
-    <div>
-      <Otsikko name="anna palautetta"/>
-      <Button funktio={props.tila[0].funktio()}
-              name={props.tila[0].nimi} />
-      <Button funktio={props.tila[1].funktio()}
-              name={props.tila[1].nimi} />
-      <Button funktio={props.tila[2].funktio()}
-              name={props.tila[2].nimi} />
-    </div>
-  )
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tilastot: [
-        {
-          nimi: "hyvä",
-          value: 0,
-          funktio: this.kasvataHyva
-        },
-        {
-          nimi: "neutraali",
-          value: 0,
-          funktio: this.kasvataNeutraali
-        },
-        {
-          nimi: "huono",
-          value: 0,
-          funktio: this.kasvataHuono
-        },
-        {
-          nimi: "keskiarvo",
-          value: 0,
-        },
-        {
-          nimi: "positiivisia",
-          value: 0,
-        }
-      ]
+      hyva: 0,
+      neutraali: 0,
+      huono: 0
+    }
+
+    this.tilastoFunktiot.laskeKeskiarvo =
+        this.tilastoFunktiot.laskeKeskiarvo.bind(this)
+    this.tilastoFunktiot.laskePositiivistenOsuus =
+        this.tilastoFunktiot.laskePositiivistenOsuus.bind(this)
+  }
+
+  tilastoFunktiot = {
+    laskeKeskiarvo: function() {
+      const ka = (this.state.hyva - this.state.huono) /
+      (this.state.hyva + this.state.neutraali + this.state.huono)
+      return ka
+    },
+
+    laskePositiivistenOsuus: function () {
+      const pos = ((this.state.hyva) / (this.state.hyva
+        + this.state.neutraali
+        + this.state.huono)) * 100
+      return pos
     }
   }
 
-  kasvataHyva = () => {
-    return () => {
-      const elementit = this.state.tilastot
-      elementit[0].value = elementit[0].value + 1
-      elementit[3].value = this.laskeKeskiarvo()
-      elementit[4].value = this.positiiviOsuus()
+  kasvataArvoa = (destination) => {
+    const value = this.state[destination]+1
 
-      this.setState({
-        tilastot: elementit
-      })
+    if (destination === "hyva"){
+      return () => {
+        this.setState({hyva: value})
+      }
+    } else if (destination === "neutraali"){
+      return () => {
+        this.setState({neutraali: value})
+      }
+    } else {
+      return () => {
+        this.setState({huono: value})
+      }
     }
-  }
-
-  kasvataNeutraali = () => {
-    return () => {
-      const elementit = this.state.tilastot
-      elementit[1].value = elementit[1].value + 1
-      elementit[3].value = this.laskeKeskiarvo()
-      elementit[4].value = this.positiiviOsuus()
-
-      this.setState({
-        tilastot: elementit
-      })
-    }
-  }
-
-  kasvataHuono = () => {
-    return () => {
-      const elementit = this.state.tilastot
-      elementit[2].value = elementit[2].value + 1
-      elementit[3].value = this.laskeKeskiarvo()
-      elementit[4].value = this.positiiviOsuus()
-
-      this.setState({
-        tilastot: elementit
-      })
-    }
-  }
-
-  laskeKeskiarvo = function() {
-    const elementit = this.state.tilastot
-    const keskiarvo = elementit[3].value = Math.abs(elementit[0].value -
-      elementit[2].value) /
-      (elementit[0].value +
-        elementit[1].value + elementit[2].value)
-
-    return keskiarvo
-  }
-
-  positiiviOsuus = function () {
-    const elementit = this.state.tilastot
-    const osuus = (elementit[0].value / (elementit[0].value +
-                  elementit[1].value + elementit[2].value) * 100)
-    return osuus
   }
 
   render() {
     return (
       <div>
-        <Palaute tila={this.state.tilastot} />
-        <Statistics tila={this.state.tilastot} />
+        <Otsikko name="anna palautetta"/>
+        <Button name="hyvä"
+          funktio={this.kasvataArvoa("hyva")} />
+        <Button name="neutraali"
+          funktio={this.kasvataArvoa("neutraali")} />
+        <Button name="huono"
+          funktio={this.kasvataArvoa("huono")} />
+        <Otsikko name="statistiikka"/>
+        <Statistics tila={this.state} funktiot={this.tilastoFunktiot} />
       </div>
     )
   }
