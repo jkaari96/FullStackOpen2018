0aa213c7b92f2d7f4f042ad56411ffc7d029ef34
jkaari96 <jaakkoakaariainen@gmail.com>
2018-11-20 12:20:53 +0200

    Exercise 2.13 finished

--- part2/countrysearch/src/App.js
+++ part2/countrysearch/src/App.js
@@ -1,23 +1,50 @@
 import React from 'react'
 import axios from 'axios'
 
-const Country = (props) => {
-  return (
-    <div>
-      {props.name}
-    </div>
-  )
-}
+class Country extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      toggled: this.props.isToggled
+    }
 
-const CountryDetailed = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <p>capital: {props.capital}</p>
-      <p>population: {props.population}</p>
-      <img src={props.flag} width="100%" alt="" />
-    </div>
-  )
+    this.clickFunction = this.clickFunction.bind(this)
+  }
+
+  clickFunction() {
+    if (this.state.toggled){
+      this.setState({toggled: false})
+    } else {
+      this.setState({toggled: true})
+    }
+  }
+
+  render () {
+    const display = () => {
+      if (!this.state.toggled){
+        return (
+          <div>
+            {this.props.name}
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <h1>{this.props.name}</h1>
+            <p>capital: {this.props.capital}</p>
+            <p>population: {this.props.population}</p>
+            <img src={this.props.flag} width="100%" alt="" />
+          </div>
+        )
+      }
+    }
+
+    return (
+      <div onClick={this.clickFunction}>
+        {display()}
+      </div>
+    )
+  }
 }
 
 class App extends React.Component {
@@ -52,16 +79,19 @@ class App extends React.Component {
     } else if (countryArray.length === 1) {
       return (
         <div>
-        {countryArray.map(country => <CountryDetailed key={country.name}
+        {countryArray.map(country => <Country key={country.name}
           name={country.name} capital={country.capital}
-          population={country.population} flag={country.flag} />)}
+          population={country.population} flag={country.flag}
+          isToggled={true} />)}
         </div>
       )
     } else{
       return (
         <div>
         {countryArray.map(country => <Country key={country.name}
-           name={country.name}/>)}
+          name={country.name} capital={country.capital}
+          population={country.population} flag={country.flag}
+          isToggled={false} />)}
         </div>
       )
     }
